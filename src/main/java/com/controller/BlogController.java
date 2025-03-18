e2060ef19735a01620879de117ff80bed088999d
Shatrughan-alt <bisenprateeksingh@gmail.com>
2025-03-18 16:13:14 +0530

    second

--- src/main/java/com/controller/BlogController.java
+++ src/main/java/com/controller/BlogController.java
@@ -2,11 +2,14 @@ package com.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,18 +34,54 @@ public class BlogController {
 	
 	 @PostMapping
 	    public ResponseEntity<BlogDTO> createBlog(@Valid @RequestBody BlogDTO blogDTO) {
-	        return ResponseEntity.status(201).body(blogService.createBlog(blogDTO));
+		 BlogDTO blog=blogService.createBlog(blogDTO);
+	        return new ResponseEntity<>(blog,HttpStatus.CREATED);
 	    }
+	@GetMapping("/{id}")
+	public ResponseEntity<BlogDTO> findBlogById(@PathVariable Long id){
+		BlogDTO blogData=blogService.findBlogById(id);
+		return new ResponseEntity<>(blogData,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteBlogById(@PathVariable Long id) {
+	    blogService.deleteBlogById(id);
+	    return ResponseEntity.ok("Blog Deleted Successfully"); 
+	}
+
+
+	@PutMapping("/{id}")
+	public ResponseEntity<BlogDTO> updateBlog(@PathVariable Long id,@Valid @RequestBody BlogDTO blogDTO){
+		BlogDTO blog=blogService.updateBlog(id,blogDTO);
+		return new ResponseEntity<>(blog,HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<BlogDTO>> findAllBlog(){
+		List<BlogDTO> list=blogService.findAllBlog();
+		return new ResponseEntity<>(list,HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	 @PostMapping("/comment")
-	    public ResponseEntity<CommentDTO> addComment(@Valid @RequestBody CommentDTO commentDTO) {
-	        return ResponseEntity.status(201).body(commentService.addComment(commentDTO));
+	 public ResponseEntity<CommentDTO> addComment(@Valid @RequestBody CommentDTO commentDTO) {
+	     return ResponseEntity.status(201).body(commentService.addComment(commentDTO));
 	    }
+	 
 	 @GetMapping("{id}/comment")
 	 public ResponseEntity<List<CommentDTO>> getCommentsByBlogId(@PathVariable Long id) {
 	     List<CommentDTO> comments = commentService.getCommentsByBlogId(id);
 	     return ResponseEntity.ok(comments);
 	 }
+	 
+	 
+	 
 
 
 }
--- src/main/java/com/service/BlogService.java
+++ src/main/java/com/service/BlogService.java
@@ -1,8 +1,13 @@
 package com.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.entity.BlogEntity;
+import com.exception.CustomException;
 import com.model.BlogDTO;
 import com.repository.BlogRepository;
 
@@ -39,8 +44,46 @@ public class BlogService {
         BlogEntity savedBlog = blogRepository.save(convertBlogDTOToEntity(blogDTO));
         return convertBlogEntityToDTO(savedBlog);
     }
+	
+	public BlogDTO findBlogById(Long id) {
+		Optional<BlogEntity> blog=blogRepository.findById(id);
+		if(blog.isPresent()) {
+			BlogDTO blogDTO=convertBlogEntityToDTO(blog.get());
+			return blogDTO;
+		}
+		else {
+			throw new CustomException("No Blog Found With The Given Id");
+		}
+	}
 
-
+	public boolean deleteBlogById(Long id) {
+		Optional<BlogEntity> blog=blogRepository.findById(id);
+		if(blog.isPresent()) {
+			blogRepository.deleteById(id);
+			return true;
+		}
+		else {
+			throw new CustomException("No Blog Found With The Given Id");
+		}
+	}
+	  public BlogDTO updateBlog(Long id,BlogDTO blogDTO) {
+		  Optional<BlogEntity> blog=blogRepository.findById(id);
+		  if(blog.isPresent()) {
+			  BlogEntity blogEntity=convertBlogDTOToEntity(blogDTO);
+			  blogRepository.save(blogEntity);
+			  return convertBlogEntityToDTO(blogEntity);
+		  }
+		  else {
+				throw new CustomException("No Blog Found With The Given Id");
+			}
+	  }
 	  
+	  public List<BlogDTO> findAllBlog(){
+		  List<BlogEntity> blogs=blogRepository.findAll();
+		  return blogs.stream().map(blog->{
+			  BlogDTO blogDTO=convertBlogEntityToDTO(blog);
+			  return blogDTO;
+		  }).collect(Collectors.toList());
+	  }
 	
 }
